e3648b1a5100cc2a6433af940e303ddf4af3396f
achenPDFTron <achen@pdftron.com>
2021-05-21 12:18:24 -0700

    put in custom code to reproduce issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,12 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  @ViewChild('viewer') private viewer!: ElementRef;
-  @ViewChild('scrollView') private scrollView!: ElementRef;
+  @ViewChild('viewer', {static: false}) private viewer: ElementRef;
+  @ViewChild('scrollView', {static: false}) private scrollView: ElementRef;
+
+  constructor() {
+
+  }
 
   ngAfterViewInit(): void {
     // @ts-ignore
@@ -20,6 +24,18 @@ export class AppComponent implements AfterViewInit {
     docViewer.setScrollViewElement(this.scrollView.nativeElement);
     docViewer.setViewerElement(this.viewer.nativeElement);
     docViewer.setOptions({ enableAnnotations: true });
-    docViewer.loadDocument('/files/webviewer-demo-annotated.pdf');
+    docViewer.loadDocument('/lib/core/webviewer-demo-annotated.pdf');
+
+    // manually append iframe
+    // const iframe = document.createElement('iframe');
+    // iframe.setAttribute('width', '100%');
+    // iframe.setAttribute('src', '/lib/core/pdf/iframe.html');
+    // iframe.setAttribute('height', 'auto');
+    // iframe.setAttribute('frameBorder', '0');
+    // iframe.addEventListener('load', this.onIframeLoaded);
+    // document.body.appendChild(iframe);
+  }
+  onIframeLoaded() {
+    alert('hi');
   }
 }
